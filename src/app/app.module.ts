6020631fba91f5264a10f252ede73e3e78b9e05a
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-09-12 11:52:20 -0500

    sin errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    AppRoutingModule,
+    SharedModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedComponent } from './shared.component';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 
 
 @NgModule({
   declarations: [
-    SharedComponent
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
