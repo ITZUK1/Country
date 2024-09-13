7dfaa294350ba028dc0900cd5c24b8d51fae6480
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-09-13 07:02:56 -0500

    se crean diferentes componentes

---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {  Routes } from '@angular/router';
+
+const Routes: Routes =[
+
+    {
+
+        
+    }
+
+
+]
+
+@NgModule({
+    imports:[],
+    exports:[],
+})
+
+export class CountriesRoutingModule {}
